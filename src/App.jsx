716a588f6b0be49fe4f9ec2666e34f4edafbb263
brijesh2392 <bp2392.dev@gmail.com>
2023-11-16 16:42:40 +0530

    singup corrrection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import { AuthContextProvider } from './context/AuthContext';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Account from './pages/Account';
-import ProtectedRoute from './components/ProtectedRoute';
-import Signup from './pages/SignUp';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Account from "./pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -13,11 +13,11 @@ function App() {
       <AuthContextProvider>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route
-            path='/account'
+            path="/account"
             element={
               <ProtectedRoute>
                 <Account />
