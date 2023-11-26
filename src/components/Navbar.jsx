5a20f8dcee9487a3fbb0c2f4a3fd17148d51728c
brijesh2392 <bp2392.dev@gmail.com>
2023-11-26 15:34:29 +0530

    login name changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { UserAuth } from '../context/AuthContext';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
   const { user, logOut } = UserAuth();
@@ -10,38 +10,38 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       await logOut();
-      navigate('/');
+      navigate("/");
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-      <Link to='/'>
-        <h1 className='text-[#d85a3b] text-4xl font-bold cursor-pointer'>
+    <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
+      <Link to="/">
+        <h1 className="text-[#d85a3b] text-4xl font-bold cursor-pointer">
           MovFLIX
         </h1>
       </Link>
       {user?.email ? (
         <div>
-          <Link to='/account'>
-            <button className='text-white pr-4'>Account</button>
+          <Link to="/account">
+            <button className="text-white pr-4">Account</button>
           </Link>
           <button
             onClick={handleLogout}
-            className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'
+            className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white"
           >
             Logout
           </button>
         </div>
       ) : (
         <div>
-          <Link to='/login'>
-            <button className='text-white pr-4'>Sign In</button>
+          <Link to="/login">
+            <button className="text-white pr-4">Login</button>
           </Link>
-          <Link to='/signup'>
-            <button className='bg-[#d85a3b] px-6 py-2 rounded cursor-pointer text-white'>
+          <Link to="/signup">
+            <button className="bg-[#d85a3b] px-6 py-2 rounded cursor-pointer text-white">
               Sign Up
             </button>
           </Link>
